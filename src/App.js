1650e7082644511e0bd307860dc92668355cba97
sferris726 <a30046430@mcla.edu>
2022-03-16 20:52:43 -0400

    couple more

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Topbar from "./components/topbar/Topbar";
+import Works from "./components/works/Works";
+import "./app.scss"
+
+
+function App() 
+{
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import "./topbar.scss"
+
+export default function Topbar() 
+{
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">intro</a>
+        </div>
+        <h1>Hello</h1>
+        <div className="right">
+          right side
+        </div>
+      </div>
+    </div>
+  )
+}
