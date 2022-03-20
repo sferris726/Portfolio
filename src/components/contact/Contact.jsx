9624efe7f2cfb480ef4d9f572669f10a684a91ba
sferris726 <a30046430@mcla.edu>
2022-03-20 19:50:58 -0400

    Contact form update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,11 +5,14 @@ import { useState } from "react";
 export default function Contact() 
 {
   const [message, setMessage] = useState(false);
+  const [nameVal, setNameVal] = useState('');
+  const [emailVal, setEmailVal] = useState('');
+  const [subjectVal, setSubjectVal] = useState('');
+  const [textVal, setTextVal] = useState('');
 
   function sendEmail(e)
   {
     e.preventDefault();
-    setMessage(true);
 
     emailjs.sendForm("service_pae4hs8", "template_ggbytfv", e.target, "jpcdAzkvD3yRd9N8X")
       .then((result) => 
@@ -20,6 +23,7 @@ export default function Contact()
     {
       console.log(error.text);
     });
+    setMessage(true);
     e.target.reset()
   }
 
@@ -31,11 +35,11 @@ export default function Contact()
       <div className="right">
         <h2>Contact Me</h2>
         <form onSubmit={sendEmail}>
-          <input type="text" placeholder="Name" name="name"></input>
-          <input type="text" placeholder="Email" name="email"></input>
-          <input type="text" placeholder="Subject" name="subject"></input>
-          <textarea placeholder="Message" name="message"></textarea>
-          <button type="submit">Send</button>
+          <input type="text" placeholder="Name" name="name" value={nameVal} onChange={e => setNameVal(e.target.value)}></input>
+          <input type="text" placeholder="Email" name="email" value={emailVal} onChange={e => setEmailVal(e.target.value)}></input>
+          <input type="text" placeholder="Subject" name="subject" value={subjectVal} onChange={e => setSubjectVal(e.target.value)}></input>
+          <textarea placeholder="Message" name="message" value={textVal} onChange={e => setTextVal(e.target.value)}></textarea>
+          <button disabled={!nameVal || !emailVal || !subjectVal || !textVal || message} type="submit">Send</button>
           {message && <span>Thank you!</span>}
         </form>
       </div>
